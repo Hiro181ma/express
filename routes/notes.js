b7d92553712daeb4b9ca45f83f38af6883a46b31
Hiro Iwai <2201355010jg@cyber-u.ac.jp>
2024-07-20 08:57:09 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -1,18 +1,26 @@
-var express= require('express');
-var router= express.Router();
+var express = require('express');
+var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
 
 // 接続情報を設定
-const {MongoClient} = require("mongodb");
-const uri= "mongodb+srv://Hiro181:7dFnDrLVfWOL7XHx@hiro181.zcdpvof.mongodb.net/?retryWrites=true&w=majority&appName=Hiro181";
-const client= new MongoClient(uri);
-router.get('/', async(req, res) =>{
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
+
+router.get('/', async (req, res) => {
 // データベース、コレクションを指定
-const database= client.db('notes');
-const notes= database.collection('notes');
+const database = client.db('notes');
+const notes = database.collection('notes');
+
 // idが１のドキュメントを取得
-const query = { id:2};
+const query = { id: 2 };
 const note = await notes.findOne(query);
+
 res.json(note);
 })
 
-module.exports= router;
+module.exports = router;
