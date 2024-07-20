98377f41e2c4e2ef0e5010de2d506d151f6f2faa
Hiro Iwai <2201355010jg@cyber-u.ac.jp>
2024-07-20 09:54:39 +0900

    last update

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,6 @@ const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
 // 接続情報を設定
-<<<<<<< HEAD
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
@@ -13,12 +12,6 @@ const client = new MongoClient(uri);
 router.use(cors());
 
 router.get('/', async (req, res) => {
-=======
-const {MongoClient} = require("mongodb");
-const uri= "***";
-const client= new MongoClient(uri);
-router.get('/', async(req, res) =>{
->>>>>>> ea982c259fe22f4caf1f33127b1453610f89ffbc
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
@@ -30,8 +23,4 @@ const note = await notes.findOne(query);
 res.json(note);
 })
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports= router;
->>>>>>> ea982c259fe22f4caf1f33127b1453610f89ffbc
